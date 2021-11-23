ea87195bb4103f118170d486448117357da42e34
fseyedmousavi <faezehseyedmousavi@gmail.com>
2021-11-23 02:55:22 -0500

    add other files

---
+++ finder.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+	printf("Hello. I am finder.\n");
+	sleep(1);
+	return 0;
+}
+
---
+++ placer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+	printf("Hello. I am placer.\n");
+	sleep(1);
+	return 0;
+}
+
